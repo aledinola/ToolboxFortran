8acd7652b880a49f1159975a811dc68bb0ac57cb
Alessandro Di Nola <40523651+aledinola@users.noreply.github.com>
2021-08-31 15:17:41 +0200

    Update loadArray.m

--- loadArray.m
+++ loadArray.m
@@ -1,7 +1,7 @@
 function x = loadArray(filename,siz)
 %Purpose: You have an array saved in a file (as a column vector) and you
 %want to read it into Matlab.
-%Esample: in Fortran you compute value function V(k,z) with dim (nk,nz)
+%Example: in Fortran you compute value function V(k,z) with dim (nk,nz)
 % You write in a file as V with dim (nk*nz,1)
 % You read the file in Matlab and reshape it into (nk,nz)
 
